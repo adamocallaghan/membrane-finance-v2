cf5022a10a1509635a9edbff601313f555b824bd
adamocallaghan <adam.ocallaghan@yahoo.ie>
2024-12-23 15:05:03 +0000

    Vaniall deployment script added

---
+++ script/DeployMorpho.s.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.20;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {Vault} from "../src/Morpho.sol";
+
+contract DeployMorpho is Script {
+    function run() external {
+        // ===================
+        // === SCRIPT VARS ===
+        // ===================
+
+        uint256 deployerPrivateKey = vm.envUint("DEPLOYER_PRIVATE_KEY");
+        string memory DEPLOYER_PUBLIC_ADDRESS = "DEPLOYER_PUBLIC_ADDRESS";
+
+        // ===========================
+        // === ABSTRACT DEPLOYMENT ===
+        // ===========================
+
+        console2.log("###########################################");
+        console2.log("########## Deploying to Abstract ##########");
+        console2.log("###########################################");
+
+        vm.createSelectFork("abstract");
+
+        // Let's go...
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        // deploy Membrane core contract
+        Morpho membraneApp = new Morpho{salt: "xyz"}(vm.envAddress(DEPLOYER_PUBLIC_ADDRESS));
+        console2.log("Membrane Address: ", address(membraneApp));
+
+        vm.stopBroadcast();
+    }
+}
